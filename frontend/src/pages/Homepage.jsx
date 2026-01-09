176af718e9735a07c7139f54e806b27a3994e5ae
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2026-01-09 17:33:31 +0530

    Just tune the padding.

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -25,7 +25,7 @@ const Homepage = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-screen-xl mx-auto p-2 sm:p-4">
+      <div className="max-w-screen-xl mx-auto p-4">
         <GoogleReviews />
         <Separator className="my-8" />
         {loading.fetchCategoriesWithItems ? (
